feb04c76b0fb49f44109e447bfa5929a5500f4d6
Mozzhakov <imozzhakov@icloud.com>
2023-02-04 20:02:17 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,6 +17,14 @@ export class App extends Component {
   };
 
   addContact = (name, number) => {
+    if (
+      this.state.contacts.find(
+        contact => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
+      alert(`${name} is already in contacts!`);
+      return;
+    }
     const newContact = {
       id: nanoid(),
       name: name,
